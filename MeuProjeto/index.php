2b7197ec6237d73849cff08f7177b0592f49d591
profetagero <alexandre_profeta@hotmail.com>
2016-10-01 16:50:06 -0300

    Modificção na linha 14

--- MeuProjeto/index.php
+++ MeuProjeto/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       // Minha nova modificação 
+       echo "Minha Modificação em PHP"; 
         ?>
     </body>
 </html>
